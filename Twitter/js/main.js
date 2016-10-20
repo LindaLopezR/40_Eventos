244ce3587ffa6a8b335bec409cce90b7952473ea
LindaLopezR <guadalupelopez.lr@gmail.com>
2016-10-20 16:48:07 +0000

    Avance de ejercicio

---
+++ Twitter/js/main.js
@@ -0,0 +1,29 @@
+var lista = document.getElementById("lista");
+var	tarea = document.getElementById("tarea");
+var	btnNuevaTarea = document.getElementById("botonAgregar");
+// EVENTOS
+tarea.addEventListener("click", comprobar);
+btnNuevaTarea.addEventListener("click", agregarTarea);
+lista.addEventListener("click", eliminarTarea);
+// FUNCIONES
+function comprobar(){
+	
+};
+function agregarTarea(){
+	var tareaUsuario = tarea.value;
+	// var nuevaTarea = document.createElement("li");
+	// var enlace = document.createElement("input");
+	// var contenido = document.createTextNode(tareaUsuario);
+	if(tareaUsuario === ""){
+		
+		// tarea.className = "error";
+		alert('vas bien');
+	}
+	// enlace.appendChild(contenido);
+	// enlace.setAttribute("type", "checkbox");
+	// nuevaTarea.appendChild(enlace);
+	// lista.appendChild(nuevaTarea);
+};
+function eliminarTarea(){
+	
+};
