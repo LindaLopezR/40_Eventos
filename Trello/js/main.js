62733f02eae84caaaf9e8a7fe706fa1d19e06d5c
LindaLopezR <guadalupelopez.lr@gmail.com>
2016-10-25 22:01:34 +0000

    Proyecto Trello y corrección Twitter

--- Trello/js/main.js
+++ Trello/js/main.js
@@ -1,52 +1,50 @@
 var btnPrincipal = document.getElementById('btnPrincipal');
 var tituloLista = document.createElement('input');
 var lista = document.getElementById('lista');
+var inicio = document.getElementById('inicio');
 btnPrincipal.addEventListener('click', desplegar);
 function desplegar(){
 	var divLista = document.createElement('div');
 	var btnFormulario = document.createElement('button');
-	document.getElementById('inicio').replaceChild(divLista, btnPrincipal);
+	var tache = document.createElement('span');
+	inicio.replaceChild(divLista, btnPrincipal);
 	lista.appendChild(divLista);
+	divLista.className = 'fnd-2';
 	divLista.appendChild(tituloLista);
 	tituloLista.setAttribute('placeholder', 'Añadir una lista');
+	tituloLista.className = 'bloque';
 	divLista.appendChild(btnFormulario);
-	btnFormulario.setAttribute('class','btn btn-primary btn-xs');
+	divLista.appendChild(tache);
+	tache.setAttribute('class','glyphicon glyphicon-remove');
 	btnFormulario.appendChild(document.createTextNode('Guardar'));
+	btnFormulario.setAttribute('class','btn btn-primary btn-xs');
 	btnFormulario.addEventListener('click', guardar);
 }
-
 function guardar(){
 	if(tituloLista.value === ''){
 		tituloLista.setAttribute('placeholder', 'Ingresa un titulo');
 		return false;
 	}
-	var divLista = document.createElement('div');
-	var contenidoTarjeta = document.createElement('textarea');
+	var divGuardar = document.createElement('div');
+	var contenidoTarjeta = document.createElement('input');
 	var h3 = document.createElement('h3');	
 	var div = document.createElement('div');
-	lista.appendChild(divLista);
-	divLista.setAttribute('class', 'fnd-1 col-xs-2');
+	lista.appendChild(divGuardar);
+	divGuardar.setAttribute('class', 'fnd-1 col-xs-2');
 	h3.innerHTML = tituloLista.value;
-	divLista.appendChild(h3);
-	divLista.appendChild(contenidoTarjeta);
+	divGuardar.appendChild(h3);
+	divGuardar.appendChild(contenidoTarjeta);
 	contenidoTarjeta.setAttribute('placeholder', 'Añadir una tarjeta');
 	contenidoTarjeta.addEventListener('click', tarjetas);
-	tituloLista.value = "";
-	divLista.appendChild(div);
+	tituloLista.value = "";	
+	function tarjetas(){
+		var btnTarjeta = document.createElement('button');
+		var divTarjeta = document.createElement('div');
+		var textoTarjeta = document.createElement('textarea');
+		divGuardar.replaceChild(textoTarjeta,contenidoTarjeta);
+		divGuardar.appendChild(textoTarjeta);
+		divGuardar.appendChild(btnTarjeta);
+		btnTarjeta.setAttribute('class','btn btn-primary btn-xs');
+		btnTarjeta.appendChild(document.createTextNode('Guardar'));
+	}
 }
-function tarjetas(){
-	var btnTarjeta = document.createElement('button');
-	divLista.appendChild(btnTarjeta);
-	btnTarjeta.appendChild(document.createTextNode('Guardar'));
-		
-	// btnTarjeta.setAttribute('class', 'btn btn-primary btn-xs');
-	// btnTarjeta.appendChild(document.createTextNode('Guardar'));
-}
-
-
-// function reemplazar(){
-//         var nuevoParrafo = document.createElement('p');
-//         nuevoParrafo.appendChild(document.createTextNode('...como queríamos demostrar.'));
-//         var viejoParrafo = document.getElementById('ej-rCh').getElementsByTagName('p')[0];
-//         document.getElementById('ej-rCh').replaceChild(nuevoParrafo,viejoParrafo);
-//     }
